81cbdaef0629b872b3b1270f807f72e4b4c61182
Dan <refear99@gmail.com>
2015-07-30 12:42:45 +0800

    init

---
+++ src/Object.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User.php
+ *
+ * Part of Refear99\EasyLeancloud.
+ *
+ * For the full copyright and license information, please view the LICENSE file.
+ *
+ * @author    refear99 <refear99@gmail.com>
+ * @copyright 2015 refear99 <refear99@gmail.com>
+ * @link      https://github.com/refear99/easy-leancloud
+ */
+
+namespace Refear99\EasyLeancloud;
+
+
+class Object
+{
+    public function test()
+    {
+        return 123;
+    }
+}
